400c0ed7e76119bad104e54ffdd15b89dbcb1cc9
Melih Gultekin <mmelihgultekin@gmail.com>
2025-09-13 10:22:32 +0200

    Revert "Send logs to test"
    This reverts commit 774a55695ed6da8660ed2671a3b2758a5135e581.

--- composeApp/src/commonMain/kotlin/com/melih/kmptemplate/screens/list/ListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/melih/kmptemplate/screens/list/ListViewModel.kt
@@ -3,7 +3,6 @@ package com.melih.kmptemplate.screens.list
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.melih.kmptemplate.data.MuseumRepository
-import com.melih.kmptemplate.shared.logging.Klog
 import com.melih.kmptemplate.shared.model.MuseumObject
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -13,12 +12,4 @@ class ListViewModel(museumRepository: MuseumRepository) : ViewModel() {
     val objects: StateFlow<List<MuseumObject>> =
         museumRepository.getObjects()
             .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
-
-    init {
-        Klog.verbose { "Verbose log sample" }
-        Klog.debug { "Debug log sample" }
-        Klog.info { "Info log sample" }
-        Klog.warn { "Warn log sample" }
-        Klog.error(throwable = IllegalArgumentException("Illegal argument")) { "Error log sample" }
-    }
 }
