4ab0f89c2dcced20eb8dd8c40b9eb4f5d1e0009b
Melih Gultekin <mmelihgultekin@gmail.com>
2026-06-18 21:26:29 +0200

    Add cleanup sh command tool

---
+++ tools/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rm -rf .idea
+./gradlew clean
+rm -rf .gradle
+rm -rf build
+rm -rf */build
+rm -rf app/iosApp/iosApp.xcworkspace
+rm -rf app/iosApp/iosApp.xcodeproj/project.xcworkspace
+rm -rf app/iosApp/iosApp.xcodeproj/xcuserdata
